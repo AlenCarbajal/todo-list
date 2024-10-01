e3e466ece57a811d4560cf83ca9788b1e2a36d05
Alen-ZIGLA <acarbajal@ziglaconsultores.com>
2024-10-01 20:24:28 -0300

    user, user route y server.js creados. inicializacion de proyecto de node.

---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const MIN_NAME_LENGTH = 2;
+const MAX_NAME_LENGTH = 255;
+
+const User = sequelize.define(
+  'User',
+  {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          is: true,
+          len: [MIN_NAME_LENGTH, MAX_NAME_LENGTH]
+        },
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        }
+    },
+    hashedPassword: {
+        type: DataTypes.STRING(64),
+        allowNull: false,
+        validate: {
+          is: /^[0-9a-f]{64}$/i,
+        },
+    }
+  },
+  {
+    // Other model options go here
+  },
+);
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
