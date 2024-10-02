b8639d07816a7bbc81e45d99ba8b7c8dc6c49cd0
AlenCarbajal <alencarbajal@gmail.com>
2024-10-02 00:12:55 -0300

    correcciones a todo model y se agregó campo id autoincremental

--- src/models/todo.js
+++ src/models/todo.js
@@ -2,19 +2,24 @@ import { DataTypes } from sequelize;
 import sequelize from '../db.js';
 
 const MIN_TITLE_LENGTH = 2;
-const MAX_NAME_LENGTH = 255;
+const MAX_TITLE_LENGTH = 255;
 
 const MIN_DESCRIPTION_LENGTH = 2;
 const MAX_DESCRIPTION_LENGTH = 2048;
 const Todo = sequelize.define(
     'Todo',
     {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
         title: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 is: /^[A-Za-zÀ-ÿ\s'.-]+$/i,
-                len: [MIN_NAME_LENGTH, MAX_NAME_LENGTH]
+                len: [MIN_TITLE_LENGTH, MAX_TITLE_LENGTH]
             }
         },
         title: {
